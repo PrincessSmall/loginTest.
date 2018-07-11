1a8de7fd3eeb0afc37495ef5250a60b913e4d6f3
李敏 <limin@limindemacbook-air.local>
2018-07-11 10:29:27 +0800

    Initial Commit

---
+++ login/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  login
+//
+//  Created by 李敏 on 2018/7/11.
+//  Copyright © 2018年 李敏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ login/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  login
+//
+//  Created by 李敏 on 2018/7/11.
+//  Copyright © 2018年 李敏. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
